fb8fe3f72143648a491ba3c5295bf80f4f252e8d
Alireza Mokhtari G <ar.mokhtari.g@gmail.com>
2022-11-16 16:08:13 +0330

    loader action added

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
-import Root from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+
 
 import ErrorPage from "./error-page";
 
@@ -18,6 +19,8 @@ const router = createBrowserRouter( [
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
