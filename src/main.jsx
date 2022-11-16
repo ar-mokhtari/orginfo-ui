98d0a85725d47caa09870a6d3b3b7aade658fba8
Alireza Mokhtari G <ar.mokhtari.g@gmail.com>
2022-11-16 16:49:30 +0330

    edit delete cancel

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,14 @@ import Root, { loader as rootLoader, action as rootAction, } from "./routes/root
 
 import ErrorPage from "./error-page";
 
-import Contact from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+
+import EditContact, { action as editAction, } from "./routes/edit";
+
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter( [
   {
@@ -22,9 +29,22 @@ const router = createBrowserRouter( [
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation, } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
@@ -6,16 +6,19 @@ export async function loader() {
   return { contacts };
 }
 export async function action() {
-  await createContact();
+  const contact = await createContact();
+  return redirect( `/contacts/${contact.id}/edit` );
 }
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-        <form id="search-form" role="search">
+          <form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
@@ -42,7 +45,16 @@ export default function Root() {
             <ul>
               {contacts.map( ( contact ) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={( { isActive, isPending } ) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                          ? "pending"
+                          : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -51,7 +63,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ) )}
             </ul>
@@ -62,7 +74,12 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
         <Outlet />
       </div>
     </>
